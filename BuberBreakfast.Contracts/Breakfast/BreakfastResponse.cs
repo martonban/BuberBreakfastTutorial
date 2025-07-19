f0dbdc61c6523f022502f514cc55397c6277d60d
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-07-19 19:07:41 +0200

    API Modelled in C# Classes

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,12 @@
+namespace Breakfast.Contracts.Breakfast;
+
+public record BreakfastReponse (
+    Guid Id,
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    DateTime LastModifiedDateTime,
+    List<string> Savory,
+    List<string> Sweet
+);
---
+++ BuberBreakfast.Contracts/Breakfast/CreateBreakfastRequest.cs
@@ -0,0 +1,10 @@
+namespace Breakfast.Contracts.Breakfast;
+
+public record CreateBreakfastRequest (
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet
+);
